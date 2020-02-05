00bf11cb5b496e81e6a20a5ea0705d4af7e29b76
Mary <maryliu.388@gmail.com>
2020-02-05 10:29:24 -0500

    Updates

---
+++ precache-manifest.b355a75a260c534557fd8c9ba7eb7fc3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21a0346d3eecf57d032f8a8ae5a6931",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "30fff79e09159fc81990",
+    "url": "/portfolio/static/css/main.50510b61.chunk.css"
+  },
+  {
+    "revision": "7c1c762a2f10d0ddc856",
+    "url": "/portfolio/static/js/2.47b667b3.chunk.js"
+  },
+  {
+    "revision": "30fff79e09159fc81990",
+    "url": "/portfolio/static/js/main.56ecc72c.chunk.js"
+  },
+  {
+    "revision": "4234e20402381065f478",
+    "url": "/portfolio/static/js/runtime-main.01d0503f.js"
+  },
+  {
+    "revision": "f973017ade6456c5d476ba25826866ff",
+    "url": "/portfolio/static/media/KpopDemo.f973017a.mov"
+  },
+  {
+    "revision": "1fffc325b44d7d0edfe4ab486fbdf381",
+    "url": "/portfolio/static/media/Pokemon.1fffc325.mov"
+  },
+  {
+    "revision": "8f9c789210de5e6addd856b50fb8a62f",
+    "url": "/portfolio/static/media/TravelGalleryDemo.8f9c7892.mov"
+  },
+  {
+    "revision": "a7ea3b6979c4a878c01c586fefa8695d",
+    "url": "/portfolio/static/media/me.a7ea3b69.png"
+  }
+]);
