dcc2fa16870dde63a8f76b0664a4eeea052087b9
Mary <maryliu.388@gmail.com>
2020-07-27 15:43:23 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ import '../styles.css';
 const Contact = () => {
     return (
         <div id="links">
-            {'>'}<a href="https://drive.google.com/file/d/1JnXyRzTZ07IBxWifyaWpi2Aa3OdxBjpD/view" target="_blank" rel="noopener noreferrer">Resume</a> |
+            {'>'}<a href="https://drive.google.com/file/d/1jmab1vhOYDhoxf_hzyscYuIgPmVmxENh/view" target="_blank" rel="noopener noreferrer">Resume</a> |
             <a href="https://github.com/hui388" target="_blank" rel="noopener noreferrer">Github</a> |
             <a href="https://www.linkedin.com/in/maryrliu" target="_blank" rel="noopener noreferrer">LinkedIn</a>{'<'}
         </div>
